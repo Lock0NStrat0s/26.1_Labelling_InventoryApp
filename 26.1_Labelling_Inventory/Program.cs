1b2a6a294e989f68e5b51f9f742bfe914714779a
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-20 23:10:39 -0600

    Add project files.

---
+++ 26.1_Labelling_Inventory/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _26._1_Labelling_Inventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
